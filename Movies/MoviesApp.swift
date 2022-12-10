a275386b4c37e29bee71d94e939ee42a3a8a4445
Fares <Mahmoudfares9394@gmail.com>
2022-12-10 23:53:20 +0200

    Initial Commit

---
+++ Movies/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by MahmoudFares on 10/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
